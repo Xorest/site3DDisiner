a8942a016a674d938c016be4e8152ceef959dbc6
Xorest <60926989+Xorest@users.noreply.github.com>
2022-05-14 15:41:01 +0300

    Add project

---
+++ site3DDisiner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testWebAspNetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
